8dea2fc7bddf39845c466ed50f1632e9635c9c19
Chen Shamir <chenshamir1203@gmail.com>
2015-07-17 12:30:18 +0300

    server_yonit

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -6,7 +6,7 @@ module.exports = function() {
     //response configuration
     app.set('json spaces',4);
     app.use(function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+        res.header('Access-Control-Allow-Origin', 'http://shenkar.html5-book.co.il/2014-2015/ws/dev_25');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header('Access-Control-Allow-Credentials', 'true');
         res.set("Content-Type", "application/json");
