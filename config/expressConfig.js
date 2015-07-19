8e043dfca162389abb63ed81eac6167d1c2ab119
Chen Shamir <chenshamir1203@gmail.com>
2015-07-19 20:23:25 +0300

    pla

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -10,7 +10,7 @@ module.exports = function() {
         /*res.header('Access-Control-Allow-Origin', 'http://localhost:8080');*/
         res.header('Access-Control-Allow-Origin',"*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Credentials', 'true');
+        /*res.header('Access-Control-Allow-Credentials', 'true');*/
         res.set("Content-Type", "application/json");
         next();
     });
