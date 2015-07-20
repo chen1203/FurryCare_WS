8df4caf49afbb039c8067ec17dcf001a8fb659bd
Chen Shamir <chenshamir1203@gmail.com>
2015-07-20 11:35:04 +0300

    try

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -8,16 +8,17 @@ module.exports = function() {
     app.use(function(req, res, next) {
         
        /* res.header('Access-Control-Allow-Origin', 'http://localhost:8080');*/
-        //res.header('Access-Control-Allow-Origin',"*");
+        res.header('Access-Control-Allow-Origin',"*");
 
 
 
-        res.header('Access-Control-Allow-Origin', 'http://shenkar.html5-book.co.il');
+       /* res.header('Access-Control-Allow-Origin', 'http://shenkar.html5-book.co.il');*/
+
 
         
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Credentials', 'false');
-        /*res.set("Content-Type", "application/json");*/
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.set("Content-Type", "application/json");
         next();
     });
 
