8be85159eaa08fa7d423fe9a4bfa17b52dbd907d
Tom Cohen <tomcohent@gmail.com>
2016-01-07 12:52:35 +0200

    origin

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -5,7 +5,7 @@ module.exports = function() {
     // response configuration
     app.set('json spaces',4);
     app.use(function(req, res, next) { 
-        res.header('Access-Control-Allow-Origin',"*");
+        res.header('Access-Control-Allow-Origin',"req.headers.origin");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.set("Content-Type", "application/json");
         next();
